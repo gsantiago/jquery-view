a489e8a44165caef17e560843805ef0debbdd4e0
Guilherme Santiago <guilherme.santiago95@gmail.com>
2016-04-17 15:32:51 +0000

    Add support for multiple elements

--- jquery-view.js
+++ jquery-view.js
@@ -8,6 +8,17 @@ var Template = require('./src/Template')
 
 $.fn.view = function (options) {
   if (!this.length) return
+
+  if (this.length > 1) {
+    var views = []
+
+    this.each(function () {
+      views.push($.fn.view.call($(this), options))
+    })
+
+    return views
+  }
+
   var view = new View(this, options)
   this.data('view', view)
   return view
